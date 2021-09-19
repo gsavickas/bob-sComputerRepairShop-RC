8de0e734fe114d5b8528882c4ee3fe515d5a5b89
Larry Ohaka <larryohaka@ymail.com>
2021-09-19 16:50:29 -0400

    user list and Create

--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -1,4 +1,15 @@
+/**
+ * Title: user-create.component.ts
+ * Modified By: Larry
+ * Date: 19 September 2021
+ * Description:Create users configuration
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from './../../shared/user.interface';
+import { UserService } from './../../shared/user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -6,10 +17,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent implements OnInit {
+  user: User;
+  userId: string;
+  form: FormGroup;
+  roles: any;
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder, private router: Router, private userService: UserService) { }
+//Reaction form for user along with the validators
   ngOnInit(): void {
+    this.form = this.fb.group({
+      userName: [null, Validators.compose([Validators.required])],
+      password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$')])],
+      firstName: [null, Validators.compose([Validators.required])],
+      lastName: [null, Validators.compose([Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required])],
+      address: [null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.required, Validators.email])]//Email validator
+    });
   }
 
-}
+  //Creates new user object and takes the field forms from the html doc
+  createUser(): void {
+    const newUser: User = {
+      userName: this.form.controls.userName.value,
+      password: this.form.controls.password.value,
+      firstName: this.form.controls.firstName.value,
+      lastName: this.form.controls.lastName.value,
+      phoneNumber: this.form.controls.phoneNumber.value,
+      address: this.form.controls.address.value,
+      email: this.form.controls.email.value,
+    };
+//Calls the User service
+    this.userService.createUser(newUser).subscribe(res =>{
+      this.router.navigate(['/users']);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+
+  cancel(): void{
+    this.router.navigate(['/users']);
+  }
+  }
--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,15 +1,60 @@
+/**
+ * Title: user-list.component.ts
+ * Modified By: Larry
+ * Date: 19 September 2021
+ * Description: user list configuration
+ */
+
+
+
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteRecordDialogComponent } from './../../shared/delete-record-dialog/delete-record-dialog.component';
+import { UserService } from './../../shared/user.service';
+import { User } from './../../shared/user.interface';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+//Data table with its array of users objects
+export class UserCreateComponent implements OnInit {
+  users: User [];
+  displayedColumns = ['userName', 'firstName', 'lastName', 'phoneNumber', 'address', 'email', 'functions'];
 
-  constructor() { }
+  //Calls the findAllUsers api and fills the user field with the users object
+  constructor(private dialog: MatDialog, private userService: UserService) {
+    this.userService.findAllUsers().subscribe(res => {
+      this.users = res['data'];
+      console.log(this.users);
+    }, err => {
+      console.log(err);
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  //Delete method that opens up the dialog of both the userId and recordId 
+  delete(userId: string, recordId: string): void {
+    const dialogRef = this.dialog.open(DeleteRecordDialogComponent, {
+      data: {
+        recordId,
+        dialogHeader: 'Delete Record Dialog',
+        dialogBody: `Are you sure you want to delete user ${recordId}?`
+      },
+      disableClose: true,
+      width: '800px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      //When confirmed it will  call the deleteUser service
+      if (result === 'confirm') {
+        this.userService.deleteUser(userId).subscribe(res => {
+          console.log(`User delete`);
+          this.users = this.users.filter(u => u._id !== userId);
+        });
+      }
+    });
+  }
 }
