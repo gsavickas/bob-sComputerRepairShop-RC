dc43e0a33cef37b1599dabf915b71db4455dfba4
Larry Ohaka <larryohaka@ymail.com>
2021-10-04 21:07:46 -0400

    invoice.js

---
+++ server/models/invoice.js
@@ -0,0 +1,24 @@
+/**
+ * Author: Prof. Richard Krasso
+ * Modified By: Larry Ohaka
+ * Date: 10/2/21
+ * Title: invoice.js
+ * 
+ */
+
+ const mongoose = require('mongoose');
+ const Schema = mongoose.Schema;
+ 
+ const lineItemDocument = require('../schemas/line-item');
+ //invoice schema
+ const invoiceSchema = new Schema({
+     userName: { type: String },
+     lineItems: [lineItemDocument],
+     partsAmount: { type: Number },
+     laborAmount: { type:Number},
+     lineItemTotal: { type: Number },
+     total: { type: Number },
+     orderDate: { type: Date, default: new Date() }
+ })
+ //exports to module
+ module.exports = mongoose.model('Invoice', invoiceSchema);
