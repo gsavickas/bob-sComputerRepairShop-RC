b21ce39e98137977cca987bd112e0bb2d908aff9
Grayton Savickas <gsavickaspro@gmail.com>
2021-10-06 21:24:39 -0500

    adds code attribution

--- src/app/shared/role.guard.spec.ts
+++ src/app/shared/role.guard.spec.ts
@@ -1,3 +1,10 @@
+/**
+ * * Title: role.guard.ts
+ * Author: Grayton Savickas
+ * Date: 10/6/21
+ * Description: role guard that prevent users other than ones with the admin role.
+ */
+
 import { TestBed } from '@angular/core/testing';
 
 import { RoleGuard } from './role.guard';
--- src/app/shared/role.guard.ts
+++ src/app/shared/role.guard.ts
@@ -1,3 +1,10 @@
+/**
+ * * Title: role.guard.ts
+ * Author: Grayton Savickas
+ * Date: 10/6/21
+ * Description: role guard that prevent users other than ones with the admin role.
+ */
+
 import { RoleService } from './services/role.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
@@ -15,9 +22,11 @@ export class RoleGuard implements CanActivate {
     return this.roleService.findUserRole(this.cookieService.get('sessionuser')).pipe(map(res =>{
       console.log(res);
      
-      
+      // check to see if the user has the admin role
       if(res['data'].role === 'admin'){
+        // if true the guard allows the re-route to protected pages
         return true;
+        // else the router will navigate to the home page
       } else {
         this.router.navigate(['/']);
         return false;
