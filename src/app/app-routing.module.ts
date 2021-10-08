95fc9aeefa06f9b8c9b6e7952ff613f40b0c176d
Grayton Savickas <gsavickaspro@gmail.com>
2021-10-08 18:55:22 -0500

    adds fixes for merge request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,12 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { ErrorComponent } from './pages/error/error.component';
 
 
-
+import { RoleListComponent } from './pages/role-list/role-list.component';
+import { RoleCreateComponent } from './pages/role-create/role-create.component';
+import { RoleDetailsComponent } from './pages/role-details/role-details.component';
+import { PurchasesByServiceGraphComponent } from './pages/purchases-by-service-graph/purchases-by-service-graph.component';
+import { RoleGuard } from './shared/role.guard';
+import {MatCheckboxModule} from '@angular/material/checkbox' 
 
 
 
